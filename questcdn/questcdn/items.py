328969ec96e9790ad4ccbe8e6e389d68c42aeeac
Satish Terala <satishkt@gmail.com>
2021-10-31 23:58:44 -0400

    setting up scrapy

--- questcdn/questcdn/items.py
+++ questcdn/questcdn/items.py
@@ -9,4 +9,16 @@ import scrapy
 class QuestcdnItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    agent_name = scrapy.Field()
+    project_name = scrapy.Field()
+    project_number=scrapy.Field()
+    bid_due_date = scrapy.Field()
+    bid_open_date= scrapy.Field()
+    estimated_start_date = scrapy.Field()
+    estimated_completion_date = scrapy.Field()
+    percent_completion = scrapy.Field()
+    contract_amt = scrapy.Field()
+    project_cntractor = scrapy.Field()
+    constr_year = scrapy.Field()
+    constr_type = scrapy.Field()
+    district = scrapy.Field()
---
+++ questcdn/questcdn/spiders/city_of_madison_spider.py
@@ -0,0 +1,34 @@
+import scrapy
+
+from questcdn.items import QuestcdnItem
+
+
+class CityOfMadisonSpider(scrapy.Spider):
+    name = "city_of_madison"
+    start_urls = ["https://cityofmadison.com/business/pw/contracts/"]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response, **kwargs):
+        for page_href in response.xpath("//table[@class='border']/tr/td/a/@href").getall():
+            yield response.follow(page_href, callback=self.parse_child_page)
+
+    def parse_child_page(self, response):
+        data_table = response.xpath("//div[@class='box']/div[@class='box_body']/table")
+        item = QuestcdnItem()
+        item['agent_name'] = self.name
+        item['project_name'] = data_table.xpath('./tr[1]/td[2]/text()').extract()
+        item['project_number'] = data_table.xpath('./tr[2]/td[2]/text()').extract()
+        item['bid_due_date'] = data_table.xpath('./tr[3]/td[2]/text()').extract()
+        item['bid_open_date'] = data_table.xpath('./tr[4]/td[2]/text()').extract()
+        item['estimated_start_date'] = data_table.xpath('./tr[5]/td[2]/text()').extract()
+        item['estimated_completion_date'] = data_table.xpath('./tr[6]/td[2]/text()').extract()
+        item['percent_completion'] = data_table.xpath('./tr[7]/td[2]/text()').extract()
+        item['contract_amt'] = data_table.xpath('./tr[8]/td[2]/text()').extract()
+        item['project_cntractor'] = data_table.xpath('./tr[9]/td[2]/text()').extract()
+        item['constr_year'] = data_table.xpath('./tr[10]/td[2]/text()').extract()
+        item['constr_type'] = data_table.xpath('./tr[11]/td[2]/text()').extract()
+        item['district'] = data_table.xpath('./tr[12]/td[2]/text()').extract()
+        yield item
